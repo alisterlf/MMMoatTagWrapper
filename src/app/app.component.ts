1e19eec358bc73fcf9b9ab95e5aa973a75c22b95
Álister Lopes Ferreira <alisterlf@gmail.com>
2018-06-08 23:51:27 -0300

    xml parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgxXml2jsonService } from 'ngx-xml2json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  duration: string;
+  encodedDuration: string;
+  encodedUrl: string;
+  height: number;
+  width: number;
+  tag: string;
+  // tslint:disable-next-line:max-line-length
+  xml = `<VAST xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="vast_2.0.1-creativeView.xsd" version="2.0"><Ad id="moat_ad"><InLine><AdSystem>2.0</AdSystem><AdTitle>Vick_-_PG1_Vick_Winter_Janeiro_2018_Brazil-23562901</AdTitle><Impression/><Creatives><Creative><Linear><Duration>00:00:15</Duration><MediaFiles><MediaFile type="application/javascript" apiFramework="VPAID" height="360" width="640" delivery="progressive"><![CDATA[https://svastx.moatads.com/iprospectpgbrazilsizmekvideo327190469710/moatwrapper.js#vast=https%3a%2f%2fbs.serving-sys.com%2fServing%3fcn%3ddisplay%26c%3d23%26pl%3dVAST%26pli%3d23562901%26PluID%3d0%26pos%3d5815%26ord%3d88178920%26cim%3d1&level1=851181&level2=MediaMath%20BR%20-%20ROS-Video&level3=23562901&level4=vpaid&slicer1=undefined&slicer2=undefined&pcode=iprospectpgbrazilsizmekvideo327190469710&spvb=1]]></MediaFile></MediaFiles></Linear></Creative></Creatives></InLine></Ad></VAST>`;
+  constructor(
+    private ngxXml2jsonService: NgxXml2jsonService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.init();
+  }
+
+  processTag(value): void {
+    this.encodedUrl = encodeURIComponent(value);
+    this.getConfig(value).subscribe(data => {
+      console.log('data', data);
+    });
+  }
+
+  getConfig(url) {
+    const headers = new HttpHeaders({ 'Content-Type': 'text/xml' }).set(
+      'Accept',
+      'text/xml'
+    );
+    return this.http.get(url, { responseType: 'text' });
+  }
+
+  private init() {
+    const obj = this.parseXml(this.xml);
+    this.duration = obj.VAST.Ad.InLine.Creatives.Creative.Linear.Duration;
+    this.height =
+      obj.VAST.Ad.InLine.Creatives.Creative.Linear.MediaFiles.MediaFile[
+        '@attributes'
+      ].height;
+    this.width =
+      obj.VAST.Ad.InLine.Creatives.Creative.Linear.MediaFiles.MediaFile[
+        '@attributes'
+      ].width;
+    this.encodedDuration = encodeURIComponent(this.duration);
+    console.log('this.encodedDuration', this.encodedDuration);
+  }
+
+  private parseXml(xml: string) {
+    const parser = new DOMParser();
+    const parsedXml = parser.parseFromString(xml, 'text/xml');
+    const obj = <any>this.ngxXml2jsonService.xmlToJson(parsedXml);
+    return obj;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { LayoutModule } from '@angular/cdk/layout';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
